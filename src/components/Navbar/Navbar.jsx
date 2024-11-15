3f5beae6ab9469db7fd8abb75723da9a5ba732ee
laxmibodke <laxmi.bodke22@spit.ac.in>
2024-11-15 18:19:47 +0530

    modify navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -16,12 +16,6 @@ const NavBar = () => {
     }
   }
   window.addEventListener("scroll", scrollHandler);
-  // useEffect(()=> {
-  //   if(CartItem.length ===0) {
-  //     const storedCart = localStorage.getItem("cartItem");
-  //     setCartItem(JSON.parse(storedCart));
-  //   }
-  // },[])
   return (
     <Navbar
       fixed="top"
